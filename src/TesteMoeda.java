7274783f0794d7d15bea419319ad02b24e1330ef
Eduardo Miguel Forato Monteiro <161526674+EduardoMiguelFM@users.noreply.github.com>
2024-03-14 09:44:47 -0300

    Update TesteMoeda.java

--- src/TesteMoeda.java
+++ src/TesteMoeda.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class TesteMoeda {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
+         Scanner leitor = new Scanner(System.in);
+        Scanner leitorTexto = new Scanner(System.in);
         Moeda moeda = new Moeda();
+
+        String nome, cpf;
+        System.out.println("Informe o cpf do cliente: ");
+        cpf = leitorTexto.nextLine();
+        System.out.println("Informe o nome do cliente: ");
+        nome = leitorTexto.nextLine();
 
         System.out.print("Informe o valor em reais: R$");
         moeda.valorReal = leitor.nextDouble();
@@ -11,9 +18,11 @@ public class TesteMoeda {
         System.out.print("Informe a cotação do dólar hoje: $");
         moeda.cotacaoDolar = leitor.nextDouble();
 
-        moeda.calcularConversao();
-        System.out.println("$" + moeda.quantidadeDolar= );
 
+        moeda.calcularConversao();
+        System.out.println("$" + moeda.quantidadeDolar);
+        System.out.println(moeda.comprarDolar("46609383871", "Eduardo Miguel"));
+        System.out.println(moeda.comprarDolar(cpf,nome));
 
 
 
